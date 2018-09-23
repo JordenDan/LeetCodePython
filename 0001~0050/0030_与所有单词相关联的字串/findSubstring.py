b420f39bf841f680295cfce40cd0006b72f66848
JordenDan <12210720045@fudan.ed.cn>
2018-09-23 10:38:07 +0800

    modify 0030 improve performance from 1800ms to 700ms

--- 0001~0050/0030_与所有单词相关联的字串/findSubstring.py
+++ 0001~0050/0030_与所有单词相关联的字串/findSubstring.py
@@ -13,17 +13,23 @@ class Solution:
         if wordLen == -1:
             return rtn
         wordCnt = len(words)
+        alreadyFoundSet = set()
         for shiftVal in range(0, wordLen):
             lst = self.cutTxt(s[shiftVal:], wordLen)
             if len(lst) < wordCnt:
                 break
-            ableLst = self.checkLst(lst, wordCnt, wordsIdx)
+            ableLst = self.checkLst(lst, wordCnt, wordsIdx, shiftVal, wordLen, alreadyFoundSet)
             rtn += [idx * wordLen + shiftVal for idx in ableLst]
         return rtn
 
-    def checkLst(self, lst, wordCnt, wordsIdx):
+    def checkLst(self, lst, wordCnt, wordsIdx, shiftVal, wordLen, alreadyFoundSet):
         rtn = []
         for shift in range(0, len(lst) - wordCnt + 1):
+            beginIdx = shiftVal + wordLen * shift
+            endIdx = beginIdx + wordCnt * wordLen
+            if (beginIdx, endIdx) in alreadyFoundSet:
+                continue
+            alreadyFoundSet.add((beginIdx, endIdx))
             wordsCheck, subLen = self.getWordsMap(lst[shift: shift + wordCnt])
             if wordsCheck == wordsIdx:
                 rtn.append(shift)
@@ -39,13 +45,11 @@ class Solution:
         return lst
 
     def getWordsMap(self, words):
-        wordsIdx = Counter()
+        wordsIdx = Counter(words)
         lenPre = -1
         for word in words:
             lenWord = len(word)
             if lenPre != -1 and lenPre != lenWord:
-                wordsIdx.clear()
-                return wordsIdx, -1
+                return Counter(), -1
             lenPre = lenWord
-            wordsIdx[word] += 1
         return wordsIdx, lenPre
